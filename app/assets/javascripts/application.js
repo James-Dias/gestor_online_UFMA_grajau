cf5e0f8d0b52ffc656dbeb4c5b129062384dcd15
James Dias <jamesdias0@gmail.com>
2019-09-12 20:08:20 -0300

    Added Navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require rails-ujs
+//= require jquery3
 //= require activestorage
 //= require bootstrap-sprockets
 //= require turbolinks
