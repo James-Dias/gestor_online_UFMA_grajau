f3ab11564befc6cb71e761105df1252a772cf3a9
James Dias <jamesdias0@gmail.com>
2019-09-16 20:27:20 -0300

    Added Users and authentication with gem devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_130917) do
+ActiveRecord::Schema.define(version: 2019_09_16_183857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,23 @@ ActiveRecord::Schema.define(version: 2019_09_12_130917) do
     t.datetime "updated_at", null: false
     t.index ["problem_id"], name: "index_situations_on_problem_id"
     t.index ["sector_id"], name: "index_situations_on_sector_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "cpf"
+    t.integer "kind"
+    t.string "occupation"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "situations", "problems"
