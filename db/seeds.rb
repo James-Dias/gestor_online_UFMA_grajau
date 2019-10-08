ac0db99ee8e780e0065d85864844d3ce9eadff9e
James Dias <jamesdias0@gmail.com>
2019-10-07 23:01:32 -0300

    Config seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+unless User.where(email: 'admin@email.com')
+  User.create(name: 'Admin', kind: 1, email: 'admin@email.com', password: 123456, password_confirmation: 123456)
+end
